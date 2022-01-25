20dc1881c10c1eab9e010af2e1db4fa7aa45a0a2
유희진 <96467030+yu-heejin@users.noreply.github.com>
2022-01-25 13:17:37 +0900

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,16 +1,21 @@
-
 import React from 'react';
-import {Text, View, Button, Image, StyleSheet} from 'react-native';
+import {Text, View, Button, Image, ImageBackground, StyleSheet} from 'react-native';
 import {Dimensions, Toolbar} from 'react-native';
 
 const App = () => {
   var {height, width} = Dimensions.get('window');
   return (
     <View style={{flex:1,backgroundColor: '#246dd5', alignItems: 'center'}}>
-      <Image
+      <ImageBackground 
         source={require('./img/backmain.png')} 
         style={{width:width,height: '100%'}}
-      />
+        >
+        <Button
+        title="Press me"
+        onPress={() => alert('Simple Button pressed')}
+        />
+      </ImageBackground>
+      
   </View>
   )
 }
