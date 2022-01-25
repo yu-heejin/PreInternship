791caaca9e1f468406400b2ac10547c7e8610811
유희진 <96467030+yu-heejin@users.noreply.github.com>
2022-01-25 20:44:42 +0900

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import {Text, View, Button, Image, ImageBackground, StyleSheet} from 'react-native';
-import {Dimensions, Toolbar} from 'react-native';
 
-const App = () => {
+import React from 'react';
+import {View, ImageBackground, StyleSheet} from 'react-native';
+import {Dimensions} from 'react-native';
+import CustomButton from './CustomButton';
+// 외부에서 패키지를 가져옴
+
+const App = () => {     //실행함수 const 함수이름 
   var {height, width} = Dimensions.get('window');
+ 
   return (
     <View style={{flex:1,backgroundColor: '#246dd5', alignItems: 'center'}}>
       <ImageBackground 
-        source={require('./img/backmain.png')} 
+        source={require('./backmain.png')} 
         style={{width:width,height: '100%'}}
         >
-        <Button
-        title="Press me"
-        onPress={() => alert('Simple Button pressed')}
-        />
+        <CustomButton></CustomButton>
       </ImageBackground>
       
   </View>
   )
 }
 
+//CSS부분
 const styles = StyleSheet.create({
   container: {
     flex : 1,
@@ -28,4 +30,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default App;    //메인 액티비티로 내보냄
