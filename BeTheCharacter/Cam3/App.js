d5500112d23c2d18f8116cd70c6c4480b5723a2b
yu-heejin <jkgt0521@naver.com>
2022-01-28 18:11:11 +0900

    frontend

---
+++ BeTheCharacter/Cam3/App.js
@@ -0,0 +1,122 @@
+import React, {Component} from 'react';
+import {Button, Text, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+
+class ProductScanRNCamera extends Component {
+  constructor(props) {
+    super(props);
+    this.camera = null;
+    this.barcodeCodes = [];
+
+    this.state = {
+      camera: {
+        type: RNCamera.Constants.Type.front,
+        // flashMode: RNCamera.Constants.FlashMode.auto,
+        //flashMode: RNCamera.Constants.FlashMode.torch,
+      },
+    };
+  }
+
+  onBarCodeRead(scanResult) {
+    if (scanResult.data != null) {
+      if (!this.barcodeCodes.includes(scanResult.data)) {
+        this.barcodeCodes.push(scanResult.data);
+        this.props.closeCamera(scanResult.data);
+        console.log('onBarCodeRead call');
+      }
+    }
+    return;
+  }
+
+  async takePicture() {
+    if (this.camera) {
+      const options = {quality: 0.5, base64: true};
+      const data = await this.camera.takePictureAsync(options);
+    }
+  }
+
+  pendingView() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'lightgreen',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text>Waiting</Text>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={(ref) => {
+            this.camera = ref;
+          }}
+          defaultTouchToFocus
+          flashMode={this.state.camera.flashMode}
+          mirrorImage={false}
+          onBarCodeRead={this.onBarCodeRead.bind(this)}
+          onFocusChanged={() => {}}
+          onZoomChanged={() => {}}
+          style={styles.preview}
+          type={this.state.camera.type}
+        />
+        <View style={[styles.overlay, styles.topOverlay]}>
+          <Text style={styles.scanScreenMessage}>Please scan the barcode.</Text>
+        </View>
+        <View style={[styles.overlay, styles.bottomOverlay]}>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    padding: 16,
+    right: 0,
+    left: 0,
+    alignItems: 'center',
+  },
+  topOverlay: {
+    top: 0,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  bottomOverlay: {
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  enterBarcodeManualButton: {
+    padding: 15,
+    backgroundColor: 'white',
+    borderRadius: 40,
+  },
+  scanScreenMessage: {
+    fontSize: 14,
+    color: 'white',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+};
+
+export default ProductScanRNCamera;
