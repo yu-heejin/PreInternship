60a29e80164a146eecd13d12749a54d54543549c
yu-heejin <jkgt0521@naver.com>
2022-02-07 13:33:49 +0900

    d

--- db/img.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const ImgSchema = new mongoose.Schema({
-    change: String,
-    notchange: String
-    //url is a String type
-})
-
-mongoose.model("imgs2", ImgSchema);
